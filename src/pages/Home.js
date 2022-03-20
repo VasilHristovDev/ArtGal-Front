b25c19d5b87d627328d1acb579bc5b8f7bb2776a
Aleksander1207 <aleksander.aleksiev12@abv.bg>
2022-03-20 13:31:16 +0200

    handling painting of the day

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,22 @@
 import MediaCard from '../components/MediaCard';
-import img from '../images/images2.jpg';
 import { useState } from 'react';
 
 const Home = () => {
     const imageOfTheDay = JSON.parse(localStorage.getItem('painting_of_the_day'));
-    console.log('img', imageOfTheDay);
+    // const [token, setToken] = useState('');
+    // const sessionToken = sessionStorage.getItem('token')
+    // if(sessionToken) {
+    //     setToken(sessionToken);
+    // }
     return (
         <main className='Main-Home'>
-            <MediaCard src={imageOfTheDay.gallery[0]} alt='painting of the day' 
-                name={imageOfTheDay.name} author={imageOfTheDay.user.name}/>
+            {(imageOfTheDay && imageOfTheDay.gallery && imageOfTheDay.gallery.length > 0) &&
+                <MediaCard src={imageOfTheDay.gallery[0]} alt='painting of the day' 
+                    name={imageOfTheDay.name} author={imageOfTheDay.user.name}/>
+            }
+            {(!imageOfTheDay || !imageOfTheDay.gallery || imageOfTheDay.gallery.length === 0) &&
+                <h1>No current image of the day</h1>
+            }
         </main>
     );
 }
